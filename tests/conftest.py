08beb059928e206d5c8d4ac58ae3e3ccbac9dcb8
Simon Kohlmeyer <simon.kohlmeyer@gmail.com>
2020-06-25 11:16:02 +0200

    Added first selenium test
    We want to add some js functionality, which needs to be tested through
    selenium. Also, some user journey tests are always good to have.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,18 @@ from app import create_app
 from config import TestConfig
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def app():
     return create_app(TestConfig())
+
+
+@pytest.fixture
+def firefox_options(firefox_options):
+    firefox_options.add_argument("--headless")
+    return firefox_options
+
+
+@pytest.fixture
+def chrome_options(chrome_options):
+    chrome_options.add_argument("--headless")
+    return chrome_options
